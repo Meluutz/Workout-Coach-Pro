f4a20e13c25181431eceeddbe16e33852460173c
Meluutz <90008074+Meluutz@users.noreply.github.com>
2026-09-07 17:13:19 -0400

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='adaptive-workout-coach-v7-3-5-mobile-interaction-polish';
+const CACHE='adaptive-workout-coach-v7-3-6-compact-coach-warmup';
 const CORE=['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png'];
 self.addEventListener('install',e=>{
   self.skipWaiting();
